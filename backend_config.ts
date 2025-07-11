fd0d098ca72cf6a8cdb7d2c1d908e2eefddaa2d4
allenmclark <104108757+allenmclark@users.noreply.github.com>
2025-07-11 18:48:05 +0000

    added backend_config to toggle local/cloud fastapi backend and also made some signup form advances

--- backend_config.ts
+++ backend_config.ts
@@ -1,5 +1,5 @@
 
 // change to process.env.CLOUD_BACKEND_URL for running cloud FastAPI deployment
 
-const api_url = process.env.LOCAL_BACKEND_URL
+export const api_url = process.env.LOCAL_BACKEND_URL
 
