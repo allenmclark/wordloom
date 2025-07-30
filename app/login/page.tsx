c8ddb8073512d1c0c8a265cb232299fce9da2687
allenmclark <104108757+allenmclark@users.noreply.github.com>
2025-07-30 18:56:32 +0000

    commit login page change

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -47,7 +47,7 @@ export default function LoginPage() {
       const data = await response.json();
 
       if (!response.ok) {
-        throw new Error(data.message || "Login failed");
+        throw new Error((data.message) || "Login failed");
       }
       if (response.ok){
         console.log(data)
